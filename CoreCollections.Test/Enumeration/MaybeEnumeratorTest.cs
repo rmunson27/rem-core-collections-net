4768082844d7db91289009bf89892ae4fce4145a
Richard Munson <108591053+rmunson27@users.noreply.github.com>
2022-11-02 19:37:17 -0400

    Added testing for single-element enumerators

---
+++ CoreCollections.Test/Enumeration/MaybeEnumeratorTest.cs
@@ -0,0 +1,43 @@
+﻿using Rem.Core.Collections.Enumeration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rem.Core.Collections.Test.Enumeration;
+
+/// <summary>
+/// Tests for the <see cref="MaybeEnumerator{T}"/> struct.
+/// </summary>
+[TestClass]
+public class MaybeEnumeratorTest
+{
+    /// <summary>
+    /// Tests an empty <see cref="MaybeEnumerator{T}"/>.
+    /// </summary>
+    [TestMethod]
+    public void TestEmpty()
+    {
+        var e = new MaybeEnumerator<int>();
+
+        Assert.ThrowsException<InvalidOperationException>(() => e.Current);
+        Assert.IsFalse(e.MoveNext());
+        Assert.ThrowsException<InvalidOperationException>(() => e.Current);
+    }
+
+    /// <summary>
+    /// Tests a <see cref="MaybeEnumerator{T}"/> wrapping a single element.
+    /// </summary>
+    [TestMethod]
+    public void TestSingleElement()
+    {
+        var e = new MaybeEnumerator<int>(4);
+
+        Assert.ThrowsException<InvalidOperationException>(() => e.Current);
+        Assert.IsTrue(e.MoveNext());
+        Assert.AreEqual(4, e.Current);
+        Assert.IsFalse(e.MoveNext());
+        Assert.ThrowsException<InvalidOperationException>(() => e.Current);
+    }
+}
---
+++ CoreCollections.Test/Enumeration/SingletonEnumeratorTest.cs
@@ -0,0 +1,30 @@
+﻿using Rem.Core.Collections.Enumeration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rem.Core.Collections.Test.Enumeration;
+
+/// <summary>
+/// Tests the <see cref="SingletonEnumerator{T}"/> struct.
+/// </summary>
+[TestClass]
+public class SingletonEnumeratorTest
+{
+    /// <summary>
+    /// Tests a <see cref="SingletonEnumerator{T}"/> instance.
+    /// </summary>
+    [TestMethod]
+    public void TestInstance()
+    {
+        var e = new SingletonEnumerator<int>(3);
+
+        Assert.ThrowsException<InvalidOperationException>(() => e.Current);
+        Assert.IsTrue(e.MoveNext());
+        Assert.AreEqual(3, e.Current);
+        Assert.IsFalse(e.MoveNext());
+        Assert.ThrowsException<InvalidOperationException>(() => e.Current);
+    }
+}
