dbb5e8bb64319bf12ad2c5f97c5d0c4771020a25
Richard Munson <108591053+rmunson27@users.noreply.github.com>
2022-11-03 18:38:02 -0400

    Added sequence equality functionality for 2D collections

---
+++ CoreCollections2D.Test/SequenceEquality2DTest.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rem.Core.Collections2D.Test;
+using Index2D = ValueTuple<long, long>;
+
+/// <summary>
+/// Tests of the
+/// </summary>
+[TestClass]
+public class SequenceEquality2DTest
+{
+    #region Constants
+    /// <summary>
+    /// A 2-dimensional array that is 2-dimensional sequence equivalent to <see cref="Array2"/>.
+    /// </summary>
+    private static readonly Index2D[,] Array = Values.Indices(2, 3);
+
+    /// <summary>
+    /// A 2-dimensional array that is 2-dimensional sequence equivalent to <see cref="Array"/>.
+    /// </summary>
+    private static readonly Index2D[,] Array2 = Values.Indices(2, 3);
+
+    /// <summary>
+    /// A 2-dimensional array that is sequence equivalent to <see cref="Array"/> and <see cref="Array2"/>, but not
+    /// 2-dimensional sequence equivalent.
+    /// </summary>
+    private static readonly Index2D[,] SequenceEquivalent = new[,]
+    {
+        { (0L, 0L), (0, 1) },
+        { (0L, 2), (1, 0) },
+        { (1L, 1), (1, 2) },
+    };
+
+    /// <summary>
+    /// A 2-dimensional array that is 2-dimensional sequence equivalent to <see cref="Array"/> and
+    /// <see cref="Array2"/> except for a mismatched element.
+    /// </summary>
+    private static readonly Index2D[,] MismatchedElement;
+
+    /// <summary>
+    /// A 2-dimensional array that contains <see cref="Array"/> and <see cref="Array2"/> but with an extra row.
+    /// </summary>
+    private static readonly Index2D[,] ExtraRow = Values.Indices(3, 3);
+
+    /// <summary>
+    /// A 2-dimensional array that contains <see cref="Array"/> and <see cref="Array2"/> but with an extra column.
+    /// </summary>
+    private static readonly Index2D[,] ExtraColumn = Values.Indices(2, 4);
+    #endregion
+
+    #region Constructor
+    static SequenceEquality2DTest()
+    {
+        MismatchedElement = Values.Indices(2, 3);
+        MismatchedElement[1, 2] = (-1, -1);
+    }
+    #endregion
+
+    #region Tests
+    /// <summary>
+    /// Tests the <see cref="SequenceEquality2D.Equals"/> method overloads.
+    /// </summary>
+    [TestMethod]
+    public void TestEquals()
+    {
+        // Array tests
+        Assert.IsTrue(SequenceEquality2D.Equals(Array, Array2));
+        Assert.IsFalse(SequenceEquality2D.Equals(Array, SequenceEquivalent));
+        Assert.IsFalse(SequenceEquality2D.Equals(Array, MismatchedElement));
+        Assert.IsFalse(SequenceEquality2D.Equals(Array, ExtraRow));
+        Assert.IsFalse(SequenceEquality2D.Equals(Array, ExtraColumn));
+
+        // Readonly array tests
+        ReadOnly2DArray<Index2D> rArray = new(Array), rArray2 = new(Array2),
+                                 rSequenceEquivalent = new(SequenceEquivalent),
+                                 rMismatchedElement = new(MismatchedElement),
+                                 rExtraRow = new(ExtraRow), rExtraColumn = new(ExtraColumn);
+        Assert.IsTrue(SequenceEquality2D.Equals(rArray, rArray2));
+        Assert.IsFalse(SequenceEquality2D.Equals(rArray, rSequenceEquivalent));
+        Assert.IsFalse(SequenceEquality2D.Equals(rArray, rMismatchedElement));
+        Assert.IsFalse(SequenceEquality2D.Equals(rArray, rExtraRow));
+        Assert.IsFalse(SequenceEquality2D.Equals(rArray, rExtraColumn));
+    }
+
+    /// <summary>
+    /// Tests the <see cref="SequenceEquality2D.GetHashCode"/> method overloads.
+    /// </summary>
+    [TestMethod]
+    public void TestGetHashCode()
+    {
+        int arrayHashCode = SequenceEquality2D.GetHashCode(Array),
+            array2HashCode = SequenceEquality2D.GetHashCode(Array2),
+            sequenceEquivalentHashCode = SequenceEquality2D.GetHashCode(SequenceEquivalent),
+            mismatchedElementHashCode = SequenceEquality2D.GetHashCode(MismatchedElement),
+            extraRowHashCode = SequenceEquality2D.GetHashCode(ExtraRow),
+            extraColumnHashCode = SequenceEquality2D.GetHashCode(ExtraColumn);
+
+        // Array tests
+        Assert.AreEqual(arrayHashCode, array2HashCode);
+        Assert.AreNotEqual(arrayHashCode, sequenceEquivalentHashCode);
+        Assert.AreNotEqual(arrayHashCode, mismatchedElementHashCode);
+        Assert.AreNotEqual(arrayHashCode, extraRowHashCode);
+        Assert.AreNotEqual(arrayHashCode, extraColumnHashCode);
+
+        // Ensure hash codes match - if so, equality tests from above also hold
+        ReadOnly2DArray<Index2D> rArray = new(Array), rArray2 = new(Array2),
+                                 rSequenceEquivalent = new(SequenceEquivalent),
+                                 rMismatchedElement = new(MismatchedElement),
+                                 rExtraRow = new(ExtraRow), rExtraColumn = new(ExtraColumn);
+        Assert.AreEqual(arrayHashCode, SequenceEquality2D.GetHashCode(rArray));
+        Assert.AreEqual(array2HashCode, SequenceEquality2D.GetHashCode(rArray2));
+        Assert.AreEqual(sequenceEquivalentHashCode, SequenceEquality2D.GetHashCode(rSequenceEquivalent));
+        Assert.AreEqual(mismatchedElementHashCode, SequenceEquality2D.GetHashCode(rMismatchedElement));
+        Assert.AreEqual(extraRowHashCode, SequenceEquality2D.GetHashCode(rExtraRow));
+        Assert.AreEqual(extraColumnHashCode, SequenceEquality2D.GetHashCode(rExtraColumn));
+    }
+    #endregion
+}
---
+++ CoreCollections2D/SequenceEquality2D.cs
@@ -0,0 +1,188 @@
+﻿using Rem.Core.Attributes;
+using Rem.Core.Collections;
+using Rem.Core.ComponentModel;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rem.Core.Collections2D;
+
+/// <summary>
+/// Provides methods for computing equality and hash codes for 2-dimensional sequences.
+/// </summary>
+public static class SequenceEquality2D
+{
+    #region Methods
+    #region Comparers
+    /// <summary>
+    /// Creates a <see cref="NestedEqualityComparer{TGeneric, TParameter}"/> that can compare
+    /// <see cref="ReadOnly2DArray{T}"/> instances based on 2-dimensional sequence equality.
+    /// </summary>
+    /// <remarks>
+    /// The returned value will compare default values as equal, and default and non-default values as unequal.
+    /// </remarks>
+    /// <typeparam name="T"></typeparam>
+    /// <returns></returns>
+    public static NestedEqualityComparer<ReadOnly2DArray<T>, T> ReadonlyArrayComparer<T>()
+        => new ReadonlyArrayComparerType<T>();
+
+    /// <summary>
+    /// Creates a <see cref="NestedEqualityComparer{TGeneric, TParameter}"/> that can compare 2-dimensional arrays of
+    /// a given type based on 2-dimensional sequence equality.
+    /// </summary>
+    /// <remarks>
+    /// The returned value will compare <see langword="null"/> values as equal, and <see langword="null"/> and
+    /// non-<see langword="null"/> values as unequal.
+    /// </remarks>
+    /// <typeparam name="T"></typeparam>
+    /// <returns></returns>
+    public static NestedEqualityComparer<T[,], T> ArrayComparer<T>() => new ArrayComparerType<T>();
+    #endregion
+
+    #region Equals
+    /// <summary>
+    /// Determines if the two <see cref="ReadOnly2DArray{T}"/> instances are equal according to their
+    /// 2-dimensional structure.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <param name="x"></param>
+    /// <param name="y"></param>
+    /// <param name="nestedComparer">
+    /// An <see cref="EqualityComparer{T}"/> to use to compare elements of type <typeparamref name="T"/>, or
+    /// <see langword="null"/> to use the default comparer.
+    /// </param>
+    /// <returns></returns>
+    public static bool Equals<T>(
+        ReadOnly2DArray<T> x, ReadOnly2DArray<T> y, IEqualityComparer<T>? nestedComparer = null)
+    {
+        // Eliminate default cases
+        if (x.IsDefault) return y.IsDefault;
+        else if (y.IsDefault) return false;
+
+        // Default the comparer if necessary
+        nestedComparer ??= EqualityComparer<T>.Default;
+
+        // Eliminate the case of mismatching dimensions
+        if (x.RowCount != y.RowCount || x.ColumnCount != y.ColumnCount) return false;
+
+        return Array2D.Enumerate(x).SequenceEqual(Array2D.Enumerate(y), nestedComparer);
+    }
+
+    /// <summary>
+    /// Determines if the 2-dimensional arrays passed in are equal according to their 2-dimensional structure.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <param name="x"></param>
+    /// <param name="y"></param>
+    /// <param name="nestedComparer">
+    /// An <see cref="EqualityComparer{T}"/> to use to compare elements of type <typeparamref name="T"/>, or
+    /// <see langword="null"/> to use the default comparer.
+    /// </param>
+    /// <returns></returns>
+    public static bool Equals<T>(T[,]? x, T[,]? y, IEqualityComparer<T>? nestedComparer = null)
+    {
+        // Default the comparer if necessary
+        nestedComparer ??= EqualityComparer<T>.Default;
+
+        // Eliminate null cases
+        if (x is null) return y is null;
+        else if (y is null) return false;
+
+        // Eliminate the case of mismatching dimensions
+        if (x.GetLength(0) != y.GetLength(0) || x.GetLength(1) != y.GetLength(1)) return false;
+
+        return Array2D.Enumerate(x).SequenceEqual(Array2D.Enumerate(y), nestedComparer);
+    }
+    #endregion
+
+    #region GetHashCode
+    /// <summary>
+    /// Gets a hash code for the <see cref="ReadOnly2DArray{T}"/> passed in based on its structure.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <param name="items"></param>
+    /// <param name="nestedComparer">
+    /// An <see cref="EqualityComparer{T}"/> to use to compare elements of type <typeparamref name="T"/>, or
+    /// <see langword="null"/> to use the default comparer.
+    /// </param>
+    /// <returns></returns>
+    /// <exception cref="StructArgumentDefaultException"><paramref name="items"/> was default.</exception>
+    public static int GetHashCode<T>(
+        [NonDefaultableStruct] ReadOnly2DArray<T> items, IEqualityComparer<T>? nestedComparer = null)
+    {
+        if (items.IsDefault) throw new StructArgumentDefaultException(nameof(items));
+        nestedComparer ??= EqualityComparer<T>.Default;
+
+        var hashCode = new HashCode();
+        hashCode.Add(items.RowCount);
+        hashCode.Add(items.ColumnCount);
+        foreach (var item in items._array) hashCode.Add(nestedComparer.GetHashCode(item!));
+        return hashCode.ToHashCode();
+    }
+
+    /// <summary>
+    /// Gets a hash code for the <see cref="ReadOnly2DArray{T}"/> passed in based on its structure.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <param name="items"></param>
+    /// <param name="nestedComparer">
+    /// An <see cref="EqualityComparer{T}"/> to use to compare elements of type <typeparamref name="T"/>, or
+    /// <see langword="null"/> to use the default comparer.
+    /// </param>
+    /// <returns></returns>
+    /// <exception cref="StructArgumentDefaultException"><paramref name="items"/> was default.</exception>
+    public static int GetHashCode<T>(T[,] items, IEqualityComparer<T>? nestedComparer = null)
+    {
+        if (items is null) throw new ArgumentNullException(nameof(items));
+        nestedComparer ??= EqualityComparer<T>.Default;
+
+        var hashCode = new HashCode();
+        hashCode.Add(items.GetLength(0));
+        hashCode.Add(items.GetLength(1));
+        foreach (var item in items) hashCode.Add(nestedComparer.GetHashCode(item!));
+        return hashCode.ToHashCode();
+    }
+    #endregion
+    #endregion
+
+    #region Types
+    private sealed class ArrayComparerType<T> : NestedEqualityComparer<T[,], T>
+    {
+        /// <inheritdoc/>
+        public override bool Equals([AllowNull] T[,] x, [AllowNull] T[,] y, IEqualityComparer<T> nestedComparer)
+        {
+            if (nestedComparer is null) throw new ArgumentNullException(nameof(nestedComparer));
+            return Equals<T>(x, y);
+        }
+
+        /// <inheritdoc/>
+        public override int GetHashCode([DisallowNull] T[,] obj, IEqualityComparer<T> nestedComparer)
+        {
+            if (nestedComparer is null) throw new ArgumentNullException(nameof(nestedComparer));
+            return GetHashCode<T>(obj, nestedComparer);
+        }
+    }
+
+    private sealed class ReadonlyArrayComparerType<T> : NestedEqualityComparer<ReadOnly2DArray<T>, T>
+    {
+        /// <inheritdoc/>
+        public override bool Equals(
+            [AllowDefault] ReadOnly2DArray<T> x, [AllowDefault] ReadOnly2DArray<T> y,
+            IEqualityComparer<T> nestedComparer)
+        {
+            if (nestedComparer is null) throw new ArgumentNullException(nameof(nestedComparer));
+            return Equals<T>(x, y);
+        }
+
+        /// <inheritdoc/>
+        public override int GetHashCode([DisallowDefault] ReadOnly2DArray<T> obj, IEqualityComparer<T> nestedComparer)
+        {
+            if (nestedComparer is null) throw new ArgumentNullException(nameof(nestedComparer));
+            return GetHashCode<T>(obj, nestedComparer);
+        }
+    }
+    #endregion
+}
