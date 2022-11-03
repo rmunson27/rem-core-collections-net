de33e54306cc1584788d3415a160de39a0708780
Richard Munson <108591053+rmunson27@users.noreply.github.com>
2022-11-03 17:09:11 -0400

    Added missing readonly 2D array testing

---
+++ CoreCollections2D.Test/AssertExtensions.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rem.Core.Collections2D.Test;
+
+/// <summary>
+/// Extensions for the <see cref="Assert"/> class.
+/// </summary>
+internal static class AssertExtensions
+{
+    /// <summary>
+    /// Asserts that the two sequences are equal.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <param name="_"></param>
+    /// <param name="expected"></param>
+    /// <param name="actual"></param>
+    /// <param name="comparer"></param>
+    /// <param name="message"></param>
+    public static void AreSequenceEqual<T>(
+        this Assert _, IEnumerable<T> expected, IEnumerable<T> actual, IEqualityComparer<T>? comparer = null,
+        string message = "")
+    {
+        comparer ??= EqualityComparer<T>.Default;
+
+        var expectedEnumerator = expected.GetEnumerator();
+        var actualEnumerator = actual.GetEnumerator();
+        bool expectedMoveNext, actualMoveNext;
+        int index = -1;
+
+        do
+        {
+            expectedMoveNext = expectedEnumerator.MoveNext();
+            actualMoveNext = actualEnumerator.MoveNext();
+            index++;
+
+            // Remove cases where one sequence exceeds the length of the other
+            if (!expectedMoveNext)
+            {
+                if (actualMoveNext)
+                {
+                    Assert.Fail(
+                        CombineMessages($"Actual sequence exceeded length of expected sequence ({index})", message));
+                }
+            }
+            else if (!actualMoveNext)
+            {
+                Assert.Fail(
+                    CombineMessages($"Expected sequence exceeded length of actual sequence ({index})", message));
+            }
+            else
+            {
+                if (!comparer.Equals(expectedEnumerator.Current, actualEnumerator.Current))
+                {
+                    Assert.Fail(
+                        CombineMessages(
+                            $"Expected value at index {index} ({expectedEnumerator.Current}) did not match"
+                                + $" actual value ({actualEnumerator.Current})",
+                            message));
+                }
+            }
+        }
+        while (expectedMoveNext);
+    }
+
+    /// <summary>
+    /// Formats a required message with an optional message.
+    /// </summary>
+    /// <param name="requiredMessage"></param>
+    /// <param name="optionalMessage"></param>
+    /// <returns></returns>
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    private static string CombineMessages(string requiredMessage, string? optionalMessage)
+        => string.IsNullOrWhiteSpace(optionalMessage)
+            ? $"{requiredMessage}."
+            : requiredMessage + $": {optionalMessage}.";
+}
--- CoreCollections2D.Test/Readonly2DArrayTest.cs
+++ CoreCollections2D.Test/Readonly2DArrayTest.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Rem.Core.Collections;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,6 +13,7 @@ namespace Rem.Core.Collections2D.Test;
 [TestClass]
 public class ReadOnly2DArrayTest
 {
+    #region Indexing
     /// <summary>
     /// Tests the <see cref="ReadOnly2DArray{T}"/> indexer.
     /// </summary>
@@ -28,7 +30,78 @@ public class ReadOnly2DArrayTest
             }
         }
     }
+    #endregion
 
+    #region Vectors
+    /// <summary>
+    /// Tests the <see cref="ReadOnly2DArray{T}.Rows"/> property.
+    /// </summary>
+    [TestMethod]
+    public void TestRows()
+    {
+        var arr = Values.Indices(2, 3);
+        var expectedRows = new[]
+        {
+            new[] { (0L, 0L), (0, 1), (0, 2) },
+            new[] { (1L, 0L), (1, 1), (1, 2) },
+        };
+
+        // Test enumeration
+        Assert.That.AreSequenceEqual(expectedRows, arr.Rows, IndexSequenceComparer);
+
+        // Test indexing
+        Assert.ThrowsException<IndexOutOfRangeException>(() => arr.Rows[-1]);
+        Assert.That.AreSequenceEqual(expectedRows[0], arr.Rows[0]);
+        Assert.That.AreSequenceEqual(expectedRows[1], arr.Rows[1]);
+        Assert.ThrowsException<IndexOutOfRangeException>(() => arr.Rows[2]);
+
+        // Test row indexing
+        for (int i = 0; i < 2; i++)
+        {
+            Assert.ThrowsException<IndexOutOfRangeException>(() => arr.Rows[i][-1]);
+            Assert.AreEqual(expectedRows[i][0], arr.Rows[i][0]);
+            Assert.AreEqual(expectedRows[i][1], arr.Rows[i][1]);
+            Assert.AreEqual(expectedRows[i][2], arr.Rows[i][2]);
+            Assert.ThrowsException<IndexOutOfRangeException>(() => arr.Rows[i][3]);
+        }
+    }
+
+    /// <summary>
+    /// Tests the <see cref="ReadOnly2DArray{T}.Columns"/> method.
+    /// </summary>
+    [TestMethod]
+    public void TestColumns()
+    {
+        var arr = Values.Indices(2, 3);
+        var expectedColumns = new[]
+        {
+            new[] { (0L, 0L), (1, 0) },
+            new[] { (0L, 1L), (1, 1) },
+            new[] { (0L, 2L), (1, 2) },
+        };
+
+        // Test enumeration
+        Assert.That.AreSequenceEqual(expectedColumns, arr.Columns, IndexSequenceComparer);
+
+        // Test indexing
+        Assert.ThrowsException<IndexOutOfRangeException>(() => arr.Columns[-1]);
+        Assert.That.AreSequenceEqual(expectedColumns[0], arr.Columns[0]);
+        Assert.That.AreSequenceEqual(expectedColumns[1], arr.Columns[1]);
+        Assert.That.AreSequenceEqual(expectedColumns[2], arr.Columns[2]);
+        Assert.ThrowsException<IndexOutOfRangeException>(() => arr.Columns[3]);
+
+        // Test column indexing
+        for (int i = 0; i < 3; i++)
+        {
+            Assert.ThrowsException<IndexOutOfRangeException>(() => arr.Columns[i][-1]);
+            Assert.AreEqual(expectedColumns[i][0], arr.Columns[i][0]);
+            Assert.AreEqual(expectedColumns[i][1], arr.Columns[i][1]);
+            Assert.ThrowsException<IndexOutOfRangeException>(() => arr.Columns[i][2]);
+        }
+    }
+    #endregion
+
+    #region Clone
     /// <summary>
     /// Tests the <see cref="ReadOnly2DArray.Clone{T}(T[,])"/> method.
     /// </summary>
@@ -45,7 +118,9 @@ public class ReadOnly2DArrayTest
         // Ensure the clone is different
         Assert.AreNotEqual(readonlyClone, readonly1);
     }
+    #endregion
 
+    #region Count
     /// <summary>
     /// Tests the count-getting methods.
     /// </summary>
@@ -69,4 +144,13 @@ public class ReadOnly2DArrayTest
         Assert.AreEqual(5, arr.ColumnCount);
         Assert.AreEqual(5L, arr.LongColumnCount);
     }
+    #endregion
+
+    #region Helpers
+    /// <summary>
+    /// An equality comparer for comparing the equality of sequences of indices.
+    /// </summary>
+    private readonly NestedEqualityComparer<IEnumerable<(long, long)>, (long, long)> IndexSequenceComparer
+        = SequenceEquality.EnumerableComparer<(long, long)>();
+    #endregion
 }
