da1759ba1e8d2426056921f8f3a0e63e5fb7766e
Richard Munson <108591053+rmunson27@users.noreply.github.com>
2023-03-31 18:57:46 -0400

    Added missing documentation for `ReadOnlyCollection<T>` constructor

--- CoreCollections/ObjectModel/ReadOnlyCollection.cs
+++ CoreCollections/ObjectModel/ReadOnlyCollection.cs
@@ -13,5 +13,10 @@ namespace Rem.Core.Collections.ObjectModel;
 /// <typeparam name="T"></typeparam>
 public class ReadOnlyCollection<T> : System.Collections.ObjectModel.ReadOnlyCollection<T>, IReadOnlyContainer<T>
 {
+    /// <summary>
+    /// Constructs a new instance of the <see cref="ReadOnlyCollection{T}"/> class wrapping the specified
+    /// <see cref="IList{T}"/>.
+    /// </summary>
+    /// <param name="list">The list to wrap in the new instance.</param>
     public ReadOnlyCollection(IList<T> list) : base(list) { }
 }
